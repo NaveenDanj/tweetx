d60fe483f5994cf4997975be1b664bcfeb859097
naveendanj <naveenhettiwaththa@gmail.com>
2024-01-15 23:18:00 +0530

    init project with tialwind css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="tw-text-3xl tw-font-bold tw-underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
